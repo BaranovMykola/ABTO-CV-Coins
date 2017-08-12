737182bed8e4c20107dc2b48719838b00df647f3
BaranovMykola <mapsg32@gmail.com>
2017-08-12 16:18:44 +0300

    Refactored main source file, renaming consts

--- Coins/CoinsSource.cpp
+++ Coins/CoinsSource.cpp
@@ -11,7 +11,6 @@
 #include <sstream>
 
 #include "Line.h"
-#include "CoinsDetection.h"
 #include "ImagePreprocessing.h"
 #include "A4CornersDetection.h"
 #include "PointsComparation.h"
@@ -22,10 +21,8 @@ using namespace cv;
 int distance = 29;
 int minLineDist = 30;
 int minGradCustom = 10;
-int marginK = 10;
 int bil_d = 30;
 int canny_low = 80;
-int hough_rho = 0;
 int hough_thresh = 70;
 int minGrad = 0;
 int imgIndex = 7; // 8, 6 crashes
@@ -34,36 +31,42 @@ int imgIndex = 7; // 8, 6 crashes
 
 
 
-std::vector<Point> getA4Corners(Mat& input)
+std::vector<Point> getA4Corners(Mat& input,
+								int bilateralDiam, 
+								int cannyThresh, 
+								int houghThresh, 
+								int minGradLines, 
+								int minGradLinesOverlap, 
+								int minPointDistance)
 {
 	reduceSize(input);
 	Mat bilateral;
-	bilateralFilter(input, bilateral, bil_d, bil_d * 2, bil_d / 2);
-	input = bilateral;
+	bilateralFilter(input, bilateral, bilateralDiam, bilateralDiam * 2, bilateralDiam / 2);
+	bilateral;
 
 	Mat imgGray;
-	cvtColor(input, imgGray, COLOR_BGR2GRAY);
+	cvtColor(bilateral, imgGray, COLOR_BGR2GRAY);
 
 	Mat edges;
 	std::cout << "Canny edge detection..." << std::endl;
-	Canny(imgGray, edges, canny_low, canny_low * 3, 3, true);
-	namedWindow("Edges", CV_WINDOW_NORMAL);
+	Canny(imgGray, edges, cannyThresh, cannyThresh * 3, 3, true);
+	//namedWindow("Edges", CV_WINDOW_NORMAL);
 	//imshow("Edges", edges);
 
 	std::vector<Vec2f> Houghlines;
 	std::cout << "Hough transfroming..." << std::endl;
-	HoughLines(edges, Houghlines, hough_rho + 1, CV_PI / 180.0, hough_thresh, 0, 0, minGrad*(CV_PI / 180));
+	HoughLines(edges, Houghlines, 1, CV_PI / 180.0, houghThresh, 0, 0, minGradLines*(CV_PI / 180));
 	auto lines = getLines(Houghlines);
 
-	Mat result = input;
-	drawLines(result, lines);
-	auto points = findRectangleCorners(lines, result.size(), minGradCustom);
+	//Mat result = input;
+	drawLines(input, lines);
+	auto points = findRectangleCorners(lines, input.size(), minGradLinesOverlap);
 
-	auto families = partitionPoints2Families(points, distance);
+	auto families = partitionPoints2Families(points, minPointDistance);
 
 	for each (auto var in families)
 	{
-		drawPoint(var, result, Scalar::all(255));
+		drawPoint(var, input, Scalar::all(255));
 	}
 
 	std::sort(families.begin(), families.end(), [](std::set<Point2f,  PointComparatorX> l, std::set<Point2f,  PointComparatorX> r) { return l.size() > r.size(); });
@@ -83,7 +86,7 @@ void changeInput(int, void* img)
 	*imgMat = imread(path);
 	Mat sourceCopy = imgMat->clone();
 
-	auto corners = getA4Corners(sourceCopy);
+	auto corners = getA4Corners(sourceCopy, bil_d, canny_low, hough_thresh, minGrad, minGradCustom, distance);
 	Mat a4corners;
 
 	Mat transMat = paperToRectangle(sourceCopy, corners, a4corners);
