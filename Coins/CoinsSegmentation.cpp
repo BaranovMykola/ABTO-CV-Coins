1b1e3af5cd5632824c50d1524a5767853d030c3f
BaranovMykola <mapsg32@gmail.com>
2017-08-27 23:22:38 +0300

    Refactored main func coins segmentation

--- Coins/CoinsSegmentation.cpp
+++ Coins/CoinsSegmentation.cpp
@@ -117,105 +117,46 @@ circleType mergeRemote(circleType& circles, int minDist, cv::Mat& dst)
 void segmentCoins(std::vector<std::pair<float, cv::Point2f>>& circles, cv::Mat source)
 {
 	Mat mask(source.size(), CV_8UC1);
-
-	/*threshold(source, mask, 170, 255, THRESH_BINARY | CV_ADAPTIVE_THRESH_MEAN_C);
-	Mat bm;
-	cvtColor(mask, bm, CV_BGR2GRAY);
-	threshold(bm, mask, 255 / 3, 255, THRESH_BINARY);
-	threshold(bm, mask, 255 / 3, 255, THRESH_BINARY_INV);*/
 	mask = getMask(source);
-	/*Mat clone = source.clone();
-	autoContrast(clone);
-	mask = truncInv(clone);*/
 
 	for (auto i : circles)
-	{
+	{	//remove explored circles from mask
 		circle(mask, i.second, i.first, Scalar::all(0), -1);
 	}
 
-	int morphSize = 8;
-	int cTresh = 200;
-	int hTresh = 9;
-	int dp = 1;
-	int bh = 1;
-
-	namedWindow("Panel");
-	createTrackbar("Morph", "Panel", &morphSize, 40);
-	createTrackbar("bh", "Panel", &bh, 40);
-	createTrackbar("dp", "Panel", &dp, 10);
-	createTrackbar("cThresh", "Panel", &cTresh, 256);
-	createTrackbar("hTresh", "Panel", &hTresh, 90);
-
-	//namedWindow("hough", CV_WINDOW_NORMAL);
-
+	
 	circleType additional;
-	while (waitKey(30) != 27)
+	Mat dst;
+	distanceTransform(mask, dst, DIST_L2, cv::DistanceTransformMasks::DIST_MASK_3, CV_32F);
+
+	threshold(dst, dst, 8, 0, THRESH_TOZERO);
+
+	Mat maskLocal;
+	non_maxima_suppression(dst, maskLocal, true);
+	Mat customMask;
+	customLocalMax(dst, customMask);
+
+	vector<Point> lmax;
+	findNonZero(maskLocal, lmax);
+	
+	for (auto i : lmax)
 	{
-		Mat dst;
-		imshow("maskLocal", mask);
-		distanceTransform(mask, dst, DIST_L2, cv::DistanceTransformMasks::DIST_MASK_3, CV_32F);
-
-		Mat dstVis;
-		normalize(dst, dstVis, 0, 1, NORM_MINMAX);
-		threshold(dst, dst, 8, 0, THRESH_TOZERO);
-		Mat t = dst;
-		//threshold(dstVis, t, 0.7, 1, THRESH_TOZERO);
-
-		/*Mat dil;
-		dilate(dstVis, dil, getStructuringElement(MORPH_ELLIPSE, Size(3, 3)));
-		Mat max = dil-dstVis;*/
-
-		Mat maskLocal;
-		non_maxima_suppression(t, maskLocal, true);
-		Mat customMask;
-		customLocalMax(t, customMask);
-
-		vector<Point> lmax;
-		findNonZero(maskLocal, lmax);
-		//vector<Vec3f> circles;
-		Mat draw = source.clone();
-		additional.clear();
-		for (auto i : lmax)
-		{
-			additional.push_back(make_pair(dst.at<float>(i), i));
-		}
-
-		auto filtered = mergeNearest(additional, 8, dst);
-		Mat full = draw.clone();
-		for (auto i : additional)
-		{
-			circle(full, i.second, i.first, Scalar(0, 0, 255), 1);
-		}
-
-		Mat fullFiltered = draw.clone();
-		for (auto i : filtered)
-		{
-			circle(fullFiltered, i.second, i.first, Scalar(0, 0, 255), 1);
-		}
-		mergeRemote(filtered, 3, full);
-		additional.clear();
-		//additional = filtered;
-		for (auto i : filtered)
-		{
-			if (canCircleBeCoin(i.second, i.first, mask.size()))
-			{
-				additional.push_back(i);
-			}
-		}
-		
-
-		for (auto i : filtered)
-		{
-			//additional.push_back(make_pair(dst.at<float>(i), i));
-			circle(draw, i.second, i.first, Scalar(0, 0, 255), 1);
-		}
-		namedWindow("draw", CV_WINDOW_NORMAL);
-		imshow("draw", draw);
-
+		additional.push_back(make_pair(dst.at<float>(i), i));
 	}
-	circles.insert(circles.end(), additional.begin(), additional.end());
 
-	return;
+	auto filtered = mergeNearest(additional, 8, dst); // merge neares circles
+	mergeRemote(filtered, 3, Mat()); // merge remoted circle (vote algorithm)
+	additional.clear();
+
+	for (auto i : filtered)
+	{
+		if (canCircleBeCoin(i.second, i.first, mask.size()))
+		{
+			additional.push_back(i);
+		}
+	}
+
+	circles.insert(circles.end(), additional.begin(), additional.end());
 }
 
 cv::Mat bilaterialBlurCoins(Mat& source)
